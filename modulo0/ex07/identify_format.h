daacd3322872d9e41fdd06c61a935280c5f09f69
Raúl Correia <1090657@isep.ipp.pt>
2017-09-22 15:56:17 +0100

    Removido codigo nao utilizado

--- modulo0/ex07/identify_format.h
+++ modulo0/ex07/identify_format.h
@@ -1,24 +1,4 @@
 #include <stdio.h>
-
-// Binary
-int isPowerOfTwo(unsigned int x) {
-  while (((x % 2) == 0) && x > 1) /* While x is even and > 1 */
-    x /= 2;
-  return (x == 1);
-}
-
-// Hex
-int validate_hex(const char *hex) {
-  while (*hex != '\0') {
-    if (*hex < '0' || *hex > '9')
-      return 0;
-    if (*hex < 'A' || *hex > 'F')
-      return 0;
-    hex++;
-  }
-  return 1;
-}
-
 // Verify if a string is Binary
 int isBinary(char *str) {
   const char li = '0', ls = '1';
