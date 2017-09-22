a9c9820b89da706c8341131eb828c0cae7fc6916
Rui Ribeiro <1150344@isep.ipp.pt>
2017-09-22 11:24:24 +0100

    Ex 11

--- modulo0/ex11/main.c
+++ modulo0/ex11/main.c
@@ -3,7 +3,7 @@
 #include "table.h"
 
 int main() {
-    printf("coef: %d\n", coef(5,2));
+    printf("coef: %d\n", coef(10,2));
 
     create_table();
     return 0;
--- modulo0/ex11/table.c
+++ modulo0/ex11/table.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 
 void create_table() {
-  int t = 0, i, f, d = 10;
+  int t = 0, i, f, d = 10, space;
   for (i = 0; i < d; i++) {
+    for (space = 1; space <= d - i; space++)
+      printf("  ");
     for (f = 0; f < d; f++) {
       t = coef(i, f);
       if (t != 0) {
-        printf("%d ", t);
+        printf("%4d ", t);
       }
     }
     printf("\n");
