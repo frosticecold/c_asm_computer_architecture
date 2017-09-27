adf1e169efa36a9edbd2610e00a029acc59a86d7
Rui Ribeiro <1150344@isep.ipp.pt>
2017-09-27 15:54:57 +0100

    Inicio ex08

---
+++ modulo1/ex08/array_sort.c
@@ -0,0 +1,25 @@
+void array_sort(int *vec, int n) {
+  int temp = 0, i, j;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n - 1; j++) {
+      if (vec[i] < vec[j]) {
+        temp = vec[i];
+        vec[i] = vec[j];
+        vec[j] = temp;
+      }
+    }
+  }
+}
+
+void array_sort_ptr(int *vec, int n) {
+  int temp = 0, i, j;
+  for (i = 1; i < n; i++) {
+    for (j = 0; j < n - i; j++) {
+      if (*(vec + j) == *(vec + j + 1)) {
+        temp = *(vec + j);
+        *(vec + j) = *(vec + j + 1);
+        *(vec + j + 1) = temp;
+      }
+    }
+  }
+}
---
+++ modulo1/ex08/array_sort.h
@@ -0,0 +1,2 @@
+void array_sort(int *vec, int n);
+void array_sort_ptr(int *vec, int n);
