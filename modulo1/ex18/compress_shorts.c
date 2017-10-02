bac2313bee2d7a85f5ad7b1413cf36675bf75fc2
Rui Ribeiro <1150344@isep.ipp.pt>
2017-10-02 17:14:36 +0100

    Ex18 completo

--- modulo1/ex18/compress_shorts.c
+++ modulo1/ex18/compress_shorts.c
@@ -1,7 +1,15 @@
+#include <stdio.h>
+
+//Left shift 4 bits
+// short 4 bits (2 bytes)
+// int 8 bits (4 bytes)
+#define pack_shorts(MSB,LSB) (((int) ((short) MSB)) & 255)<<4 | (((short) LSB)&255)
+
 void compress_shorts(short *shorts, int n_shorts, int *integers) {
-    int i;
+    int i,j=0;
     for(i=0; i<n_shorts; i++) {
-        integers[i] = shorts[i] + shorts[i+1];
+        integers[j] = pack_shorts(shorts[i+1], shorts[i]); 
+        j++;
         i++;
     }
 }
