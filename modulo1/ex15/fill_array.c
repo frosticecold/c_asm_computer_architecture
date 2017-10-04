dee5557f6750d22b6564609e264d1d4af68f8fb7
Raúl Correia <1090657@isep.ipp.pt>
2017-10-04 15:09:16 +0100

    modificado ex15 fill_array

--- modulo1/ex15/fill_array.c
+++ modulo1/ex15/fill_array.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <time.h>
 
 /*
     Fills an array with pseudo random values
@@ -6,6 +7,7 @@
 void fill_array(int *vec, int n) {
   int i;
   const int MAX_VALUE = 21;
+  srand(time(NULL));
   for (i = 0; i < n; i++) {
     vec[i] = (rand() % MAX_VALUE);
   }
