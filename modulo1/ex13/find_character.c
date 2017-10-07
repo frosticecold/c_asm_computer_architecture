8f7404116b121b61df92deffaa442fd4d65c739f
Rui Ribeiro [1150344] <1150344@isep.ipp.pt>
2017-10-07 18:57:27 +0000

    Remover import nao utilizado

--- modulo1/ex13/find_character.c
+++ modulo1/ex13/find_character.c
@@ -1,12 +1,10 @@
-#include <string.h>
-
 void find_character(char *str, char c, int *vec) {
   int i, j = 0;
   //Percorrer a string ate ao fim
   for (i = 0; str[i] != '\0'; i++) {
     //Se a letra fornecida for igual a uma da string
     if (c == str[i]) {
-      //Index da letra na string Ã© guardada no vetor
+      //Index da letra na string é guardada no vetor
       vec[j] = i;
       j++;
     }
