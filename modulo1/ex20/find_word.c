6ca3cd08a4057b0ef5a4ee7f88fe8c4fb4d2bd03
Rui Ribeiro <1150344@isep.ipp.pt>
2017-10-08 19:38:22 +0100

    Codigo confirmado e comentado

--- modulo1/ex20/find_word.c
+++ modulo1/ex20/find_word.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 
 char *find_word(char *str, char *word, char *initial_addr) {
-  //lstous(str);
-  //lstous(word);
+  // lstous(str);
+  // lstous(word);
 
   int dif = 'a' - 'A';
   int i, j = 0, index, found = 0;
-  while (str[j] != '\0' && str!=initial_addr) {
+  while (str[j] != '\0' && str != initial_addr) {
     j++;
   }
   index = j;
-  //Enquanto não chegarmos ao fim da string
+  // Enquanto não chegarmos ao fim da string
   while (str[index] != '\0') {
-    //Se a letra na string for igual à letra inicial da palavra a procurar
-    if (str[index] == word[0] || str[index] + dif == word[0] || str[index] - dif == word[0]) {
-      // || str[index] + dif == word[0] || str[index] - dif == word[0]
+    // Se a letra na string for igual à letra inicial da palavra a procurar
+    if (str[index] == word[0] || str[index] + dif == word[0] ||
+        str[index] - dif == word[0]) {
       // Encontrar a palavra total
       i = 0;
       found = 1;
-      //Então vamos percorrer a palavra e comparar se é igual
-      //Se não for igual parámos
-      //E procuramos a seguinte
+      // Então vamos percorrer a palavra e comparar se é igual
+      // Se não for igual parámos
+      // E procuramos a seguinte
       while (word[i] != '\0') {
         if (str[index + i] != word[i]) {
           found = 0;
@@ -30,17 +30,17 @@ char *find_word(char *str, char *word, char *initial_addr) {
         i++;
       }
     }
-    //SE encontrar-mos então parámos o ciclo
+    // SE encontrar-mos então parámos o ciclo
     if (found == 1) {
       break;
     }
     index++;
   }
-  //Se encontrar-mos retornamos o endereço da palavra
+  // Se encontrar-mos retornamos o endereço da palavra
   if (found == 1) {
     return &str[index];
   } else {
-    //Se não é porque não existe
+    // Se não é porque não existe
     return NULL;
   }
 }
