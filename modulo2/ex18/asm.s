ed70bac7e112ee628773198e755d42d5c2741f8c
Raúl Correia <1090657@isep.ipp.pt>
2017-10-17 16:55:29 +0100

    comentado ex18

--- modulo2/ex18/asm.s
+++ modulo2/ex18/asm.s
@@ -19,23 +19,29 @@ function: #
 
 positive:
     shr %ebx
+    # Se houver carry então é impar positivo
     jc odd_pos
 
+    # Senão é par positivo
     movl $1, %eax
     jmp fim
 
 negative:
     shr %ebx
+    # Se houver carry então é ímpar negativo
     jc odd_neg
 
+    # Se não é par negativo
     movl $2, %eax
     jmp fim
 
 odd_pos:
+    # É impar positivo
     movl $3, %eax
     jmp fim
 
 odd_neg:
+    # é impar negativo
     movl $4, %eax
     jmp fim
 
