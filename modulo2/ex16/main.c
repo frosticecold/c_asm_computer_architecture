4c1a9691b8d5ddb162a6dd5dd1881205b31b93ab
Raúl Correia <1090657@isep.ipp.pt>
2017-10-21 22:25:45 +0100

    ex16 refinado

--- modulo2/ex16/main.c
+++ modulo2/ex16/main.c
@@ -4,23 +4,23 @@
 int i=2;
 int main() {
 
-    
+    printf("Para a= 10, b=5 (declarado em assembly)\n");
     int result = function();
     //Para i=2 a=10, b = 5;
     // 1*2
     // 2 + (2*2)
     // 6
-    printf("Para i=%d: %d\n",i,result);
+    printf("Para i=%d: Resultado:%d\n",i,result);
 
     i=3;
     result = function();
     //6 + (3*2)
     // 12
-    printf("Para i=%d: %d\n",i,result);
+    printf("Para i=%d: Resultado:%d\n",i,result);
     
     i=4;
     result = function();
     //12 + (4*2)
     // 20
-    printf("Para i=%d: %d\n",i,result);
+    printf("Para i=%d: Resultado:%d\n",i,result);
 }
