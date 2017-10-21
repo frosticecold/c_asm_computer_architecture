02e997eaae468bd330b46908beab310dd87424c9
Raúl Correia <1090657@isep.ipp.pt>
2017-10-21 15:29:37 +0100

    ex05 refinado e comentado

--- modulo2/ex05/asm.s
+++ modulo2/ex05/asm.s
@@ -10,10 +10,13 @@ swapBytes:
     pushl %ebp
     movl %esp,%ebp
 
-    mov num,%ax
-    mov %al,%bh
-    mov %ah,%bl
-    mov %bx,%ax
+    movl $0,%eax
+    movl $0,%ebx
+
+    movw num,%ax # mover o número a trocar para ax
+    movb %al,%bh # trocar os low bytes para os highbytes
+    movb %ah,%bl # trocar os high bytes para os low bytes
+    movw %bx,%ax # mover o resultado para ax
 
     movl %ebp,%esp
     popl %ebp
--- modulo2/ex05/main.c
+++ modulo2/ex05/main.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include "asm.h"
 
-short num = 16385;
-short res = 0;
+short int num = 16385;
+short int res = 0;
 
 int main(){
 
+    printf("\nCaso de teste 1: \n");
     //O número após troca tem que ser 320
-    
+    // 16385
+    //Em binário : 0100 0000 0000 0001
     printf("Número antes da troca:%hd\n",num);
     res = swapBytes();
     //Supostamente para 16385 é 320
+    // Em binário : 0000 0001 0100 0000
     printf("Número após a troca é:%hd\n",res);
 
+    printf("\nCaso de teste 2:\n");
+    num = 20000;
+    // 20000
+    //Em binário : 0100 1110 0010 0000
+    printf("Número antes da troca:%hd\n",num);
+    res = swapBytes();
+    //Após a troca
+    //Res em binário : 0010 0000 0100 1110
+    //Resultado = 8270
+    printf("Número após a troca é:%hd\n",res);
 
 }
