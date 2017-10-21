2637fde9c80b8272b69c9483c2b7f8128f22a11c
Raúl Correia <1090657@isep.ipp.pt>
2017-10-21 22:23:50 +0100

    ex14 refinado

--- modulo2/ex14/main.c
+++ modulo2/ex14/main.c
@@ -6,11 +6,12 @@ int main() {
 
    // A + (B * C) / D
    op=1000;
-   
+   printf("Para ((((((%d -1) * 3) + 12) / 3) + 5) - %d) \n",op,op);
    int result = function();
-   printf("%d\n",result); // resultado é 8
+   printf("Resultado: %d\n",result); // resultado é 8
 
    op = 2000;
+   printf("\nPara ((((((%d -1) * 3) + 12) / 3) + 5) - %d) \n",op,op);
    result = function();
-   printf("%d\n",result); // resultado é 8
+   printf("Resultado: %d\n",result); // resultado é 8
 }
