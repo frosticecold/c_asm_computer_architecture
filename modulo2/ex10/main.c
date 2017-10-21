04e8c6eccc19ac2257c0325a77b0a489b6c5b96b
Raúl Correia <1090657@isep.ipp.pt>
2017-10-21 22:18:39 +0100

    refinado ex10

--- modulo2/ex10/main.c
+++ modulo2/ex10/main.c
@@ -10,13 +10,19 @@ int main() {
   op32a = 100000;
   op32b = 200000;
   // Resultado 300 000
+  printf("A verificar se flags de carry ou overflow são ativas:\n");
+  printf("-1 Overflow\n");
+  printf("-2 Carry \n");
+
   int result = sum2intscheck();
+  printf("\nPara %d e %d\n",op32a,op32b);
   printf("%d\n", result);
 
   op32a = 500000;
   op32b = 10000;
   // Rsultado 510 000s
   result = sum2intscheck();
+  printf("\nPara %d e %d\n",op32a,op32b);
   printf("%d\n", result);
 
   op32a = 2147483647;
@@ -24,6 +30,7 @@ int main() {
   //2147483647 + 10
   //Existe overflow, a flag vai ativar
   result = sum2intscheck();
+  printf("\nPara %d e %d\n",op32a,op32b);
   printf("%d\n", result);
 
   op32a = -2147483647;
@@ -31,6 +38,7 @@ int main() {
   //-2147483647 - 1
   //Aqui vai dar carry
   result = sum2intscheck();
+  printf("\nPara %d e %d\n",op32a,op32b);
   printf("%d\n", result);
 
 }
