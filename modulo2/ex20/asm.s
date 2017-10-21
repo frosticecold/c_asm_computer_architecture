f63e987b29bad0394bc0fe32c81e80bff5a69ab4
Raúl Correia <1090657@isep.ipp.pt>
2017-10-21 19:46:35 +0100

    ex20

--- modulo2/ex20/asm.s
+++ modulo2/ex20/asm.s
@@ -17,15 +17,16 @@ f:
     movl i, %ebx
     movl j, %ecx
 
-    cmpl %ebx, %ecx
+    cmpl %ebx, %ecx # (i==j)
     je equalf
 
+    # senão h = i +j
     addl %ebx, %ecx
     movl %ecx, %eax
     
     jmp fim
 
-equalf:
+equalf: # h = i-j
     subl %ecx, %ebx
     movl %ebx, %eax
 
@@ -40,16 +41,16 @@ f2:
     movl j, %ebx
 
     cmpl %ebx, %eax
-    jle f2inc
+    jle f2inc # se i <= j
 
-    inc %ebx
-    mul %ebx
+    inc %ebx # incrementar o i
+    imull %ebx # multiplicar i por j
 
     jmp fim
 
-f2inc:
-    inc %eax
-    mul %ebx
+f2inc: # se i <= j
+    inc %eax # incrementar o i
+    imull %ebx # h= i * j
 
     jmp fim
 
@@ -58,31 +59,31 @@ f3:
     pushl %ebp
     movl %esp, %ebp
 
-    movl i, %eax
+    movl i, %eax 
     movl j, %ebx
     movl i, %ecx
 
-    cmpl %ebx, %eax
-    jge f3inc
-    jl f3other
+    cmpl %ebx, %eax # se i >= j
+    jge f3inc 
+    jl f3other # senão
 
     jmp fim
 
-f3inc:
-    addl %ebx, %eax
-    inc %ecx
-    movl $0, %edx
-    div %ecx
+f3inc: 
+    addl %ebx, %eax # h = i + j
+    inc %ecx # g = i + 1
+    movl $0, %edx # limpar o registo edx
+    idivl %ecx # r = h/g
 
     jmp fim
 
 f3other:
-    mul %ebx
+    imull %ebx # h = i*j
 
-    addl %ecx, %ebx
-    addl $2, %ebx
+    addl %ecx, %ebx # g = i + j
+    addl $2, %ebx #  g= g + 2
 
-    div %ebx
+    idivl %ebx # r = h/g
     jmp fim
 
 f4:
@@ -94,26 +95,35 @@ f4:
     movl j, %ebx
     movl j, %ecx
 
-    addl %eax, %ebx
+    addl %eax, %ebx # i + j
     movl $10, %edx
-    cmpl %edx, %ebx
+    cmpl %edx, %ebx # se i + j > 10
 
-    jg f4greater
+    jg f4greater # então h = 2 * i
 
-    movl %ecx, %eax
+    movl %ecx, %eax # se não h=j/3
     movl $3, %ebx
     movl $0, %edx
-    div %ebx
+    idivl %ebx
 
     jmp fim
 
 f4greater:
-    movl $2, %ebx
-    mul %ebx
+    movl $2, %ebx 
+    imull %ebx # h = 2 * i
 
     jmp fim
 
+overflow:
+    movl $-1,%eax
+    
+    movl %ebp, %esp
+    popl %ebp
+    
+    ret
+
 fim: # fim
+    jo overflow
     # epilogue
     movl %ebp, %esp
     popl %ebp
