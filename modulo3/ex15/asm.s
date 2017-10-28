4364700b7a9f87fa7fa25076c5c4ccde2a4b8df6
Raúl Correia <1090657@isep.ipp.pt>
2017-10-29 00:23:00 +0100

    ex15

---
+++ modulo3/ex15/asm.s
@@ -0,0 +1,64 @@
+.section .data
+    .global ptrvec
+.section .text
+    .global array_sort
+
+array_sort:
+    pushl %ebp
+    movl %esp,%ebp
+    
+    movl num,%ecx
+    decl %ecx
+    movl ptrvec,%esi
+outerloop:
+    pushl %eax
+    pushl %ecx
+    pushl %edx
+    call innerfunction
+    popl %edx
+    popl %ecx
+    popl %eax
+    loop outerloop
+
+outer_exit:
+    movl %ebp,%esp
+    popl %ebp
+
+    ret
+
+innerfunction:
+    pushl %ebp
+    movl %esp,%ebp
+
+    pushl %ebx
+    pushl %esi
+    pushl %edi
+
+    movl $0,%edx  # indice do esi
+    movl num,%ecx # ecx é o contador para o loop
+    decl %ecx
+    movl ptrvec,%esi
+for_inner_loop:
+    movl (%esi,%edx,4),%eax
+    incl %edx
+    movl (%esi,%edx,4),%ebx
+    cmpl %eax,%ebx
+    jl swap
+    loop for_inner_loop
+    jmp inner_end
+swap:
+    decl %edx
+    movl %ebx,(%esi,%edx,4)
+    incl %edx
+    movl %eax,(%esi,%edx,4)
+    loop for_inner_loop
+inner_end:
+    popl %edi
+    popl %esi
+    popl %ebx
+
+    movl %ebp,%esp
+    popl %ebp
+
+    ret
+
---
+++ modulo3/ex15/main.c
@@ -0,0 +1,18 @@
+#include "asm.h"
+#include <stdio.h>
+
+int num = 5;
+ int array[5] = {5, 4, 3, 2, 1};
+//int array[5] = {6, 1, 2, 4, 3};
+int *ptrvec;
+int main() {
+  ptrvec = array;
+
+  int i;
+  array_sort();
+  printf("{ ");
+  for (i = 0; i < num; i++) {
+    printf("%d%s", array[i], (i < num - 1) ? ", " : "}\n");
+  }
+  return 0;
+}
