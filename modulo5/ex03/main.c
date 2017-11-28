56e280476e2b1dd6ee4a2bb5261053d71ebd820a
Raúl Correia <1090657@isep.ipp.pt>
2017-11-28 17:15:24 +0000

    comentarios ex03

--- modulo5/ex03/main.c
+++ modulo5/ex03/main.c
@@ -4,18 +4,17 @@
 #include "save_data.h"
 #include "student.h"
 #include "change_address.h"
-#define N_S 5
 int main() {
 
-  Student s;
-  s.age = 10;
-  s.number = 1;
-  strncpy(s.name,"José",80);
-  strncpy(s.address,"changeme",120);
-  printstruct(&s);
-  printf("\n===\n\n");
-  char nadr[120] = "ABCDEFGHIJKL";
-  change_address(&s,nadr);
-  printstruct(&s);
+  Student s;                          //Student s
+  s.age = 10;                         //Age 10
+  s.number = 1;                       //Number 1
+  strncpy(s.name,"José",80);          //strncpy José para o nome
+  strncpy(s.address,"changeme",120);  //strncpy Address
+  printstruct(&s);                    //Imprimir a struct
+  printf("\n===\n\n");                //Espaço
+  char nadr[120] = "ABCDEFGHIJKL";    //new address
+  change_address(&s,nadr);            //Change address
+  printstruct(&s);                    //Imprimir a estrutura
   return 0;
 }
