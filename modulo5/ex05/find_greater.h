d451acfa4ea500f0121f4558a5f3d8ee31b41b42
Raúl Correia <1090657@isep.ipp.pt>
2017-11-28 17:30:58 +0000

    esqueleto do ex05

---
+++ modulo5/ex05/find_greater.h
@@ -0,0 +1,5 @@
+#ifndef FIND_GREATER_H
+#define FIND_GREATER_H
+#include "student.h"
+int find_greater(Student *s, int minimum, int *greater_grades);
+#endif
---
+++ modulo5/ex05/student.h
@@ -0,0 +1,11 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+typedef struct {
+    int age;
+    int number;
+    int grades[10];
+    char name[80];
+    char address[120];
+    } Student;
+#endif
