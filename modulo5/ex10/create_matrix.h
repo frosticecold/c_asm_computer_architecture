8da0b4def302dafd8af6470792f0f58bb471e70f
Rui Ribeiro <1150344@isep.ipp.pt>
2017-11-29 15:35:04 +0000

    Ex10 memoria dinamica

---
+++ modulo5/ex10/create_matrix.h
@@ -0,0 +1,4 @@
+#ifndef CREATE_MATRIX_H
+#define CREATE_MATRIX_H
+int **create_matrix(int y,int k);
+#endif
--- modulo5/ex10/main.c
+++ modulo5/ex10/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "search_matrix.h"
+#include "create_matrix.h"
 
 int main() {
 
-  int v1[4] = {57, 82, 25, 38};
-  int v2[4] = {48, 16, 10, 29};
-  int v3[4] = {32, 7, 64, 42};
-  int v4[4] = {35, 23, 39, 50};
-  int *m[4] = {v1, v2, v3, v4};
+  int v[16] = {57, 82, 25, 38,48, 16, 10, 29,32, 7, 64, 42,35, 23, 39, 50};
+
+  int **m = create_matrix(4,4);
+
+  int i,j,k=0;
+  for(i=0;i<4;i++) {
+    for(j=0;j<4;j++) {
+      m[i][j] = v[k++];
+    }
+  }
   
   printf("%d\n", search_matrix(m,4,4,23));
   printf("%d\n", search_matrix(m,4,4,11));
