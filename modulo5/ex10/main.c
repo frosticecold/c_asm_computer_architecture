43596ced61afb21c5c7568a7993a04a52f7a8493
Raúl Correia <1090657@isep.ipp.pt>
2017-11-29 20:30:23 +0000

    free + comentarios

--- modulo5/ex10/main.c
+++ modulo5/ex10/main.c
@@ -5,10 +5,13 @@
 
 int main() {
 
+  //Vetor com dados iniciais
   int v[16] = {57, 82, 25, 38,48, 16, 10, 29,32, 7, 64, 42,35, 23, 39, 50};
 
+  //Criar matriz 4x4
   int **m = create_matrix(4,4);
 
+  //Copiar os valores para a matriz
   int i,j,k=0;
   for(i=0;i<4;i++) {
     for(j=0;j<4;j++) {
@@ -16,8 +19,15 @@ int main() {
     }
   }
   
+  //Procurar pelo numero 23;
   printf("%d\n", search_matrix(m,4,4,23));
+  //Procurar pelo numero nao existente
   printf("%d\n", search_matrix(m,4,4,11));
   
+  //Libertar o espaço alocado
+  for(i=0; i < 4; i++){
+    free(m[i]);
+  }
+  free(m);
   return 0;
 }
