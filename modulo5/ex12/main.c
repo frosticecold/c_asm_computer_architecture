39c7fe3d9ea4e1a25723d3c2352e35e9d7239fa6
Raúl Correia <1090657@isep.ipp.pt>
2017-11-29 20:28:14 +0000

    adicionado free, comentarios

--- modulo5/ex12/main.c
+++ modulo5/ex12/main.c
@@ -5,11 +5,14 @@
 
 int main() {
 
+  //Vetor inicial
   int v[16] = {57, 82, 25, 38,48, 16, 10, 29,32, 7, 64, 42,35, 23, 39, 50};
 
+  //Criar as duas matrizes
   int **m = create_matrix(4,4);
   int **m1 = create_matrix(4,4);
 
+  //Copiar os valores dos vetores para as matrizes
   int i,j,k=0;
   for(i=0;i<4;i++) {
     for(j=0;j<4;j++) {
@@ -18,6 +21,7 @@ int main() {
     }
   }
 
+  //Somar as matrzies
   int **m2 = sum_matrixes(m,m1,4,4);
 
   for(i=0;i<4;i++) {
@@ -28,6 +32,16 @@ int main() {
   }
   printf("\n");
   
+  //Libertar o espaço alocado pelas matrizes nos vetores
+  for(i=0; i < 4; i++){
+    free(m[i]);
+    free(m1[i]);
+    free(m2[i]);
+  }
+  //Libertar o espaço alocado pelas matrizes
+  free(m);
+  free(m1);
+  free(m2);
   
   return 0;
 }
