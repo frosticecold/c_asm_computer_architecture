9deda52fd01b199cd40694ac871da25b6790a51a
Raúl Correia <1090657@isep.ipp.pt>
2017-12-05 21:39:06 +0000

    ex10

--- modulo5/ex10/main.c
+++ modulo5/ex10/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "search_matrix.h"
 #include "create_matrix.h"
+#define Y 4
+#define K 4
 
 int main() {
 
@@ -9,23 +11,25 @@ int main() {
   int v[16] = {57, 82, 25, 38,48, 16, 10, 29,32, 7, 64, 42,35, 23, 39, 50};
 
   //Criar matriz 4x4
-  int **m = create_matrix(4,4);
+  int **m = create_matrix(Y,K);
 
   //Copiar os valores para a matriz
   int i,j,k=0;
-  for(i=0;i<4;i++) {
-    for(j=0;j<4;j++) {
+  for(i=0;i<Y;i++) {
+    for(j=0;j<K;j++) {
       m[i][j] = v[k++];
     }
   }
   
   //Procurar pelo numero 23;
-  printf("%d\n", search_matrix(m,4,4,23));
+  int num = 23;
+  printf("Existe o número %d: %d\n",num, search_matrix(m,Y,K,num));
   //Procurar pelo numero nao existente
-  printf("%d\n", search_matrix(m,4,4,11));
+  num = 11;
+  printf("Existe o número %d: %d\n",num, search_matrix(m,Y,K,num));
   
   //Libertar o espaço alocado
-  for(i=0; i < 4; i++){
+  for(i=0; i < Y; i++){
     free(m[i]);
   }
   free(m);
