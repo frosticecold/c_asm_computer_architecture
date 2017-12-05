f9b68f6539192d32e4030a5240d0164ba93921b3
Raúl Correia <1090657@isep.ipp.pt>
2017-12-05 21:27:27 +0000

    ex06 comentarios

--- modulo5/ex06/main.c
+++ modulo5/ex06/main.c
@@ -2,7 +2,7 @@
 #include "s1.h"
 #include "save_data.h"
 
-void printstruct(s1 *s){
+void printstruct(s1 *s){    //Método para imprimir a estrutura s1
     printf("s.i=%d\n",s->i);
     printf("s.c=%c\n",s->c);
     printf("s.j=%d\n",s->j);
@@ -11,13 +11,13 @@ void printstruct(s1 *s){
 }
 int main(){
     s1 str;
-    s1 *s = &str;
-    save_data(s,200,'A',400,'Z');
-    printstruct(s);
+    s1 *s = &str;   //Apontador para a estrutura s1
+    save_data(s,200,'A',400,'Z');//Guardar a informação na estrutura s1
+    printstruct(s);//Imprimir a estrutura
 
     printf("\n======\n\n");
-    save_data(s,1337,'B',8888,'X');
-    printstruct(s);
+    save_data(s,1337,'B',8888,'X');//Guardar a informação na estrutura s2
+    printstruct(s);//Imprimir a estrutura
 
 
     return 0;
--- modulo5/ex06/save_data.s
+++ modulo5/ex06/save_data.s
@@ -15,16 +15,16 @@
 
     movl 8(%ebp),%eax   # mover a estrutura para eax
     movl 12(%ebp),%edx  # mover vi para edx
-    movl %edx,I_OFFSET(%eax)    # mover valor de vi para o offset I
+    movl %edx,I_OFFSET(%eax)    # mover valor de vi para o campo I da estrutura s
 
     movl 16(%ebp),%edx  # mover o valor de vc para edx
-    movb %dl,C_OFFSET(%eax) # mover o valor de vc para o offset C
+    movb %dl,C_OFFSET(%eax) # mover o valor de vc para o campo C da estrutura s
 
     movl 20(%ebp),%edx  # mover o valor de vj para edx
-    movl %edx,J_OFFSET(%eax)    # mover o valor de vj para o offset J
+    movl %edx,J_OFFSET(%eax)    # mover o valor de vj para o campo J da estrutura s
 
     movl 24(%ebp),%edx  # mover o valor de vd para edx
-    movb %dl,D_OFFSET(%eax)
+    movb %dl,D_OFFSET(%eax) # mover o valor de vd para o campo D da estrutura s
     # callee save
     # epilogo
     movl %ebp,%esp
