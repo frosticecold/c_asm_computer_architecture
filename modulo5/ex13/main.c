0cf32fe9e708c48f79c7eb97fc4f85f8d1745e4f
Rui Ribeiro <1150344@isep.ipp.pt>
2017-12-05 16:29:20 +0000

    Comentarios ex13

--- modulo5/ex13/main.c
+++ modulo5/ex13/main.c
@@ -4,24 +4,36 @@
 
 int main() {
   
+  // Guardar na stack o numero 21
   push(21);
+  // Guardar na stack o numero 22
   push(22);
+  // Guardar na stack o numero 23
   push(23);
+  // Apresentar a estrutura
   display();
+  // Apresentar o tamanho da estrutura
   size();
 
+  // Remover da stack o numero 23
   int res = pop();
   printf("Popped value: %d\n",res);
   display();
   size();
+
+  // Remover da stack o numero 22  
   res = pop();
   printf("Popped value: %d\n",res);
   display();
   size();
+  
+  // Remover da stack o numero 21
   res = pop();
   printf("Popped value: %d\n",res);
   display();
   size();
+  
+  // Stack vazia  
   res = pop();
   printf("Popped value: %d\n",res);
   display();
