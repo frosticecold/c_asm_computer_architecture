2d6ab13fb93de2523bedb8551fccc8fb8c92cac7
Raúl Correia <1090657@isep.ipp.pt>
2017-12-05 21:31:41 +0000

    ex08 comentarios

--- modulo5/ex08/main.c
+++ modulo5/ex08/main.c
@@ -6,15 +6,18 @@ int main() {
   char str[80] = "ola1234567";
 
   char *s = NULL;
-  s=create_str(str);
+  s = create_str(str);
   if (s != NULL) {
     printf("%s\n", s);
     free(s);
   }
   /*
-    É possível retornar um endereço de memória para o vetor de chars, pois consegue-se dínamicamente alocar memória
+    É possível retornar um endereço de memória para o vetor de chars, pois
+    consegue-se dínamicamente alocar memória
     necessária para o vetor de chars com o uso de malloc/calloc.
-    Caso não seja possível alocar memória, o malloc/calloc retorna NULL.
+    Caso não seja possível alocar memória, o malloc/calloc retorna NULL, isto é,
+    não é possível alocar memória suficiente para guardar a informaçao da string
+    em memória.
   */
   return 0;
 }
