bb5fb89c1198c417c8a6c1aecf0282f8fa33c02b
Raúl Correia <1090657@isep.ipp.pt>
2017-12-05 16:18:12 +0000

    Comentários ex01

--- modulo5/ex01/main.c
+++ modulo5/ex01/main.c
@@ -4,14 +4,14 @@
 #include <stdlib.h>
 int main() {
 
-  Student student;
-  Student *p = &student;
-  char name[80] = "José";
-  char address[120] = "Avenida abc";
-  save_data(p, 10, 1, name, address);
+  Student student;                  //Declarar estáticamente um student
+  Student *p = &student;            //Apontador para student
+  char name[80] = "José";           //Nome
+  char address[120] = "Avenida abc";//Morada
+  save_data(p, 10, 1, name, address);//Vamos guardar em student a idade, o número, o nome e a morada
 
-  printf("Name:%s\n",p->name);
-  printf("Age:%d Number:%d\n",p->age,p->number);
-  printf("Address: %s\n",p->address);
+  printf("Name:%s\n", p->name);     //Imprimir Nome do estudante
+  printf("Age:%d Number:%d\n", p->age, p->number);//Imprimir:Idade e número de um estudante
+  printf("Address: %s\n", p->address);//Imprimir a morada de um estudante
   return 0;
 }
