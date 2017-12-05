b1e704264aa544d3e94f7bfd492ae596c43a67ab
Raúl Correia <1090657@isep.ipp.pt>
2017-12-05 21:33:50 +0000

    ex09 refinado

--- modulo5/ex09/create_matrix.c
+++ modulo5/ex09/create_matrix.c
@@ -5,9 +5,12 @@ int **create_matrix(int y, int k) { // matriz YxK
   int **matrix = NULL; // inicializar apontador de apontadores a null
   matrix = (int **)malloc(y * sizeof(int *)); // inicializar o vetor com y
                                               // linhas
-  int i;
-  for (i = 0; i < y; i++) {                     // Para as y linhas
-    matrix[i] = (int *)calloc(k, sizeof(int)); // Reservar espaço para cada vetor na matriz
+
+  if (matrix != NULL) {
+    int i;
+    for (i = 0; i < y; i++) { // Para as y linhas
+      matrix[i] = (int *)calloc(k, sizeof(int)); // Reservar espaço para cada vetor na matriz
+    }
   }
   return matrix;
 }
