284e03285c3659a73d2446b3c75e889d078a9a12
Raúl Correia <1090657@isep.ipp.pt>
2017-12-05 21:24:54 +0000

    ex05 comentarios

--- modulo5/ex05/main.c
+++ modulo5/ex05/main.c
@@ -8,24 +8,25 @@
 int main() {
 
   Student student;
-  Student *s = &student; // Apontador para a estrutura
+  Student *s = &student;              // Apontador para a estrutura
   char nome[80] = "Ana Maria";
   char address[120] = "Do not change me";
-  save_data(s,15, 1, nome, address);
+  save_data(s,15, 1, nome, address);  //Guardar info na estrutura
   
   int vec[10] =  {18,16,13,14,17,19,12,10,17,14};
   int *new_grades = vec;
-  save_grades(&student,new_grades);
+  save_grades(&student,new_grades); //Guardar as notas na estrutura
   
-  printstruct(&student);
+  printstruct(&student);            //Imprimir a estrutura
 
-  int vec1[10];
-  int *greater_grades = vec1;
+  int vec1[10];                     //Vetor que vai guardar as notas maiores que uma data nota
+  int *greater_grades = vec1;       //Apontador para esse vetor
 
-  int res = find_greater(&student, 15, greater_grades);
-  printf("\nThere are %d grades greater than 15. \n",res);
+  int nota = 15;                    //Nota a comparar
+  int res = find_greater(&student, nota, greater_grades);//Procurar as notas maiores que o valor da nota definido anteriormente
+  printf("\nThere are %d grades greater than %d. \n",res,nota);
 
-  printf("\nGrades greater than 15: ");
+  printf("\nGrades greater than %d: ",nota);
   for(int i=0; i<res; i++) {
     printf("%d ",greater_grades[i]);
   }
